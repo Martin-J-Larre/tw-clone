a7f91b9f0a9b8b2a5f8d1edd08b82aa04192d4a7
Martin-J-Larre <martinjlarre@gmail.com>
2022-10-23 22:45:32 -0400

    #6 Feed component started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Feed from "./components/Feed/Feed";
 import SidebarLeft from "./components/SidebarLeft/SidebarLeft";
 
 function App() {
   return (
     <div className="App">
       <SidebarLeft />
+      <Feed />
     </div>
   );
 }
---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,9 @@
+import styles from './feed.module.css'
+
+const Feed = () => {
+  return (
+    <div>Feed</div>
+  )
+}
+
+export default Feed
