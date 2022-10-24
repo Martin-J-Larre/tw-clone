237c92be131199aacc2bd5a7a9d1e0e453f5905a
Martin-J-Larre <martinjlarre@gmail.com>
2022-10-23 22:28:14 -0400

    #5 btn sidebar Left finished

--- src/components/SidebarLeft/SidebarLeft.js
+++ src/components/SidebarLeft/SidebarLeft.js
@@ -3,18 +3,19 @@ import HomeIcon from '@mui/icons-material/Home';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import TagIcon from '@mui/icons-material/Tag';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import styles from './sidebarLeft.module.css';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Button } from '@mui/material';
+import styles from './sidebarLeft.module.css';
 
 const SidebarLeft = () => {
   return (
     <div className={styles.flex}>
       <TwitterIcon />
-      <NavLink text="Home"  Icon={HomeIcon} />
+      <NavLink active text="Home"  Icon={HomeIcon} />
       <NavLink text="Explore" Icon={TagIcon} />
       <NavLink text="Notifications" Icon={NotificationsNoneIcon} />
       <NavLink text="Messages" Icon={MailOutlineIcon} />
@@ -22,8 +23,7 @@ const SidebarLeft = () => {
       <NavLink text="List" Icon={ListAltIcon} />
       <NavLink text="Profile" Icon={PermIdentityIcon} />
       <NavLink text="More" Icon={MoreHorizIcon} />
-      
-
+      <Button variant='outlined' className={styles.twButton} fullWidth>Tweet</Button>
     </div>
   )
 }
--- src/components/SidebarLeft/siderbarLeftComponents/NavLink.js
+++ src/components/SidebarLeft/siderbarLeftComponents/NavLink.js
@@ -1,7 +1,7 @@
 import styles from './navLink.module.css'
-const NavLink = ({text, Icon}) => {
+const NavLink = ({active,text, Icon}) => {
   return (
-    <div className={styles.links}>
+    <div className={styles.links + ' ' + (active ? styles.linkActive : '')}> 
       <Icon className={styles.icon}/>
       <h2 className={styles.h2}>{text}</h2>
     </div>
