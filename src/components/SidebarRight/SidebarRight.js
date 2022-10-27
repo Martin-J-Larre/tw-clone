ec6875d1fbf1113efccdbc7d79850216565e56d4
Martin-J-Larre <martinjlarre@gmail.com>
2022-10-27 01:27:11 -0400

    #11 sidebar right finished

--- src/components/SidebarRight/SidebarRight.js
+++ src/components/SidebarRight/SidebarRight.js
@@ -1,6 +1,6 @@
 import styles from './sidebarRight.module.css';
 import {
-    TwitterTimeLineEmbed,
+    TwitterTimelineEmbed,
     TwitterShareButton,
     TwitterTweetEmbed
 } from 'react-twitter-embed'
@@ -9,9 +9,18 @@ import { Search } from '@mui/icons-material';
 const SidebarRight = () => {
     return (
         <section className={styles.sidebar}>
-            <div className={styles.input}>
-                <Search />
-                <input placeholder="Search Twitter" type="text" />
+            <div className={styles.inputContainer}>
+                <Search className={styles.searchIcon}/>
+                <input className={styles.input} placeholder="Search Twitter" type="text" />
+            </div>
+            <div className={styles.happeningContainer}>
+                <h2 className={styles.h2}>What's Happening</h2>
+                <TwitterTweetEmbed 
+                tweetId={'933354946111705097'} />
+                <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="saurabhnemade"
+                options={{height: 500}} />
             </div>
         </section>
     )
